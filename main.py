ffe3159483b5e2eea8fd5e380edb508335c3ef45
AbduAref <arefabdu55@gmail.com>
2021-12-19 17:54:05 +0000

    Added main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame, sys,random
 
 def draw_floor():
-    screen.blit(floor_surface,(floor_x_position,900))
-    screen.blit(floor_surface,(floor_x_position+576,900))
+    screen.blit(floor_surface,(floor_x_pos,900))
+    screen.blit(floor_surface,(floor_x_pos+576,900))
 
 def create_pipe():
     random_pipe_position = random.choice(pipe_height)
@@ -31,25 +31,68 @@ def check_collison(pipes):
     if bird_rect.top <= -100 or bird_rect.bottom >= 900:
         return False
     return True
+
+
+def rotate_bird(bird):
+	new_bird = pygame.transform.rotozoom(bird,-bird_movement * 3,1)
+	return new_bird
+
+def bird_animation():
+	new_bird = bird_frames[bird_index]
+	new_bird_rect = new_bird.get_rect(center = (100,bird_rect.centery))
+	return new_bird,new_bird_rect 
+
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)),True,(255,255,255)) #colour of text
+        score_rect = score_surface.get_rect(center = (288,100))
+        screen.blit(score_surface,score_rect)
+    if game_state =='game_over':
+        score_surface = game_font.render(f'Score: {int(score)}',True,(255,255,255)) #colour of text
+        score_rect = score_surface.get_rect(center = (288,100))
+        screen.blit(score_surface,score_rect)
+
+        high_score_surface = game_font.render(f' High score: {int(high_score)}',True,(255,255,255)) #colour of text
+        high_score_rect = high_score_surface.get_rect(center = (288,850))
+        screen.blit(high_score_surface,high_score_rect)
+
+def update_score(score,high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
 pygame.init()
 screen = pygame.display.set_mode((576,1024)) #screen
 clock = pygame.time.Clock() # this just helps limit frame rate
-
+game_font = pygame.font.Font('04B_19.TTF',40)
 # variables
 gravity = 0.25
 bird_movement = 0
 game_active = True
+score = 0
+high_score = 0
 
-bg_surface = pygame.image.load('assets/background-day.png').convert() # convert just makes it better to run
-bg_surface =  pygame.transform.scale2x(bg_surface) #double the size of the image upscales
+bg_surface = pygame.image.load('assets/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
 
 floor_surface = pygame.image.load('assets/base.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
-floor_x_position = 0
+floor_x_pos = 0
 
-bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
-bird_surface = pygame.transform.scale2x(bird_surface)
+bird_downflap = pygame.transform.scale2x(pygame.image.load('assets/bluebird-downflap.png').convert_alpha())
+bird_midflap = pygame.transform.scale2x(pygame.image.load('assets/bluebird-midflap.png').convert_alpha())
+bird_upflap = pygame.transform.scale2x(pygame.image.load('assets/bluebird-upflap.png').convert_alpha())
+bird_frames = [bird_downflap,bird_midflap,bird_upflap]
+bird_index = 0
+bird_surface = bird_frames[bird_index]
 bird_rect = bird_surface.get_rect(center = (100,512))
+
+BIRDFLAP = pygame.USEREVENT + 1
+pygame.time.set_timer(BIRDFLAP,200)
+
+#bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert_alpha()
+#bird_surface = pygame.transform.scale2x(bird_surface)
+#bird_rect = bird_surface.get_rect(center = (100,512))
 
 pipe_surface = pygame.image.load('assets/pipe-green.png')
 pipe_surface = pygame.transform.scale2x(pipe_surface)
@@ -58,7 +101,8 @@ SPAWNPIPE = pygame.USEREVENT #triggered by timer
 pygame.time.set_timer(SPAWNPIPE,1200)
 pipe_height = [400,600,800] #position of pipes
 
-
+game_over_surface = pygame.transform.scale2x(pygame.image.load('assets/message.png').convert_alpha())
+game_over_rect = game_over_surface.get_rect(center=(288,512))
 while True:
   for event in pygame.event.get():  #Looking for all event that are happening (clicking keys)
     if event.type == pygame.QUIT: #or game_active== False:
@@ -69,29 +113,49 @@ while True:
             bird_movement = 0 #disables gravity momentarily 
             bird_movement -= 12
         if event.key == pygame.K_SPACE and game_active == False:
-            game_active =True
+            game_active = True
             pipe_list.clear()
-            bird_rect.clear = (100,512)
+            bird_rect.center = (100,512)
             bird_movement=0
+            score = 0
+
+
+        if event.type == BIRDFLAP:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index = 0
+            bird_surface,bird_Rect = bird_animation()
+
     if event.type == SPAWNPIPE:
         pipe_list.extend(create_pipe())
 
   # This block is being always redrawn....
   screen.blit(bg_surface,(0,0))
+
   if game_active:
     bird_movement += gravity # increasing number
+    rotated_bird = rotate_bird(bird_surface)
     bird_rect.centery += bird_movement
-    screen.blit(bird_surface,bird_rect)
+    screen.blit(rotated_bird,bird_rect)
     game_active = check_collison(pipe_list)
+
     # Pipes
     pipe_list = move_pipes(pipe_list)
     draw_pipes(pipe_list)
+
+    score += 0.01
+    score_display('main_game')
+  else:
+      screen.blit(game_over_surface,game_over_rect)
+      high_score = update_score(score,high_score)
+      score_display('game_over')
   #Floor
-  floor_x_position -=1
+  floor_x_pos -=1
   # This just has two floors and when floor is too far off, change in x axis
   draw_floor()
-  if floor_x_position <= - 576:
-      floor_x_position = 0
+  if floor_x_pos <= - 576:
+      floor_x_pos = 0
    # puts a surface on another surface
   pygame.display.update()
   clock.tick(120) #can only run under 120 fps
