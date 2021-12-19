a7a53707d88e42d3fe6712603761fc6fa48e589c
AbduAref <arefabdu55@gmail.com>
2021-12-19 14:36:08 +0000

    Added basic code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((576,1024)) #screen
+
+
+while True:
+  for event in pygame.event.get():  #Looking for all event that are happening (clicking keys)
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+  pygame.display.update()
